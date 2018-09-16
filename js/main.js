a449295de2785396e46eff9ed9c3b8f08b7e7a7a
HarryTate <harryjamestate@gmail.com>
2018-09-16 20:32:44 +0100

    add Block numbers

--- js/main.js
+++ js/main.js
@@ -1,30 +1,32 @@
+// Block 1
 var increaseNumberByOne = 0;
 function increaseNumber(){
   increaseNumberByOne++;
   document.getElementById("btn-increment").innerHTML = increaseNumberByOne;
 }
 
+// Block 2
 function getDate(){
   var date = new Date();
   document.getElementById("date").innerHTML = date.toDateString();
 }
-
+// Block 3
 function changeWord(){
    var word = "new word";
    document.getElementById("word").innerHTML = word;
 }
-
+// Block 4
 function randomNumber(){
   var number = Math.floor((Math.random() * 100) + 1);
   document.getElementById("randomNumber").innerHTML = number;
 }
-
+// Block 5
 function generateWord(){
   var words = ["cheese", "butter", "steak", "chips", "fruit", "cola"];
   var randomWord = words[Math.floor(Math.random() * words.length)];
   document.getElementById("randomWord").innerHTML = randomWord;
 }
-
+// Block 6
 function matchNumber(numberToBeMatched){
    if(numberToBeMatched == 2){
      return  "yes";
@@ -32,15 +34,17 @@ function matchNumber(numberToBeMatched){
      return "Not Equal";
    }
 }
-numberToBeMatched = 2;
-document.getElementById("numberMatch").innerHTML = matchNumber(numberToBeMatched);
-
+function activateMatchNumber(){
+  numberToBeMatched = 2;
+  document.getElementById("numberMatch").innerHTML = matchNumber(numberToBeMatched);
+}
+// Block 7
 function chooseNumber(choice){
    var user;
    user = choice;
    document.getElementById("printChoice").innerHTML = user ;
 }
-
+// Block 8
 function greaterThanTen(num){
   if(num > 10){
     return "Number is greater than 10";
@@ -53,7 +57,7 @@ function greaterThanTen(num){
 function activateNumCheck(){
   document.getElementById("numberGreaterOrLessThan").innerHTML = greaterThanTen(10);
 }
-
+// Block 9
 function numberBetweenTwoValues(num){
   if (num < 10 && num > 5){
     return "Number is between 5 and 10";
@@ -65,7 +69,7 @@ function numberBetweenTwoValues(num){
 function activateNumberBetween(){
   document.getElementById("numberBetweenTwoValues").innerHTML = numberBetweenTwoValues(11);
 }
-
+// Block 10
 function whatIsSizeOfNumber(num){
   if(num < 5){
     return "Number is less than 5";
@@ -81,19 +85,19 @@ function whatIsSizeOfNumber(num){
 function activateWhatIsSizeOfNumber(){
   document.getElementById("whatIsSizeOfNumber").innerHTML = whatIsSizeOfNumber(11);
 }
-
+// Block 11
 function takeInput(){
   var input = document.getElementById("userInput").value;
     document.getElementById("output").innerHTML = input;
   }
-
+// Block 12
 function addFiveToNumber(){
   var input = document.getElementById("addFive").value;
   input = parseInt(input);
   input += 5;
   document.getElementById("addFiveResult").innerHTML = input;
 }
-
+// Block 13
 function switchCase(animal){
   switch(animal){
     case "cat":
@@ -108,6 +112,7 @@ function switchCase(animal){
     default:
       return "no Sound";
   }
+
 }
 function activateSwitchCase(){
   document.getElementById("switchCase").innerHTML = switchCase("cat");
