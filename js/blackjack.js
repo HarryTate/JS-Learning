483a6d54cb2aade7eb79d97b9882f4f22a4bfc2c
HarryTate <harryjamestate@gmail.com>
2018-11-05 21:49:30 +0000

    Added regex practice

--- js/blackjack.js
+++ js/blackjack.js
@@ -5,32 +5,32 @@ function startGame(){
 
 }
 function pickACard() {
-    var displayHand = document.getElementById("hand");
-    displayHand.style.display = "block";
-    var playerCard = Math.floor((Math.random() * 13) + 1);
-    cardsInPlayersHand.push(playerCard);
-    total = playerCard + total;
-    if (total > 21) {
-        playerLose();
-    } else {
-        console.log(cardsInPlayersHand);
-        document.getElementById("playerHandTotal").innerHTML = total;
-        document.getElementById("playerHand").innerHTML = cardsInPlayersHand;
-    }
+  var displayHand = document.getElementById("hand");
+  displayHand.style.display = "block";
+  var playerCard = Math.floor((Math.random() * 13) + 1);
+  cardsInPlayersHand.push(playerCard);
+  total = playerCard + total;
+  if (total > 21) {
+    playerLose();
+  } else {
+    console.log(cardsInPlayersHand);
+    document.getElementById("playerHandTotal").innerHTML = total;
+    document.getElementById("playerHand").innerHTML = cardsInPlayersHand;
+  }
 }
 
 function playerLose() {
-    console.log("your total was" + total);
-    document.getElementById("losingCards").innerHTML = cardsInPlayersHand ;
-    document.getElementById("losingScore").innerHTML = total;
-    total = 0;
-    cardsInPlayersHand = [];
-    var displayHand = document.getElementById("hand");
-    displayHand.style.display = "none";
-    var inGame = document.getElementById("inGame");
-    inGame.style.display = "none";
-    var gameLost = document.getElementById("gameLost");
-    gameLost.style.display = "block";
+  console.log("your total was" + total);
+  document.getElementById("losingCards").innerHTML = cardsInPlayersHand ;
+  document.getElementById("losingScore").innerHTML = total;
+  total = 0;
+  cardsInPlayersHand = [];
+  var displayHand = document.getElementById("hand");
+  displayHand.style.display = "none";
+  var inGame = document.getElementById("inGame");
+  inGame.style.display = "none";
+  var gameLost = document.getElementById("gameLost");
+  gameLost.style.display = "block";
 }
 
 function restartGame(){
