3e636a75ff3e45b97e30ca4cbfeebdc6cfa6ee81
HarryTate <harryjamestate@gmail.com>
2018-11-10 01:01:52 +0000

    CHanged functions to anon function ES6

--- js/regex.js
+++ js/regex.js
@@ -1,56 +1,54 @@
-document.getElementById("regex").addEventListener("click", usingTestMethod);
-function usingTestMethod(){
+document.getElementById("regex").addEventListener("click",
+ () => {
   let str = "Here is a string that we can test";
   let regex = /that we can test/;
   //See if regex os cotained in str
   let result = regex.test(str);
   document.getElementById("regex").innerHTML = result;
   return result;
-}
-document.getElementById("multipleRegex").addEventListener("click", checkMultipleRegex);
-function checkMultipleRegex(){
+});
+document.getElementById("multipleRegex").addEventListener("click",
+() => {
   let str = "Jake Paul Ian Albert Theo";
   let regex = /Paul|Ian|Albert/;
   let result = regex.test(str);
   document.getElementById("multipleRegex").innerHTML = result;
-}
+});
 
-document.getElementById("ignoreCaseRegex").addEventListener("click", ignoreCaseRegex);
-function ignoreCaseRegex(){
+document.getElementById("ignoreCaseRegex").addEventListener("click", () => {
   let str = "The coLD man went to the market";
   //  '/i' ignores the case of the letters
   let regex = /cold/i;
   let result = regex.test(str);
   document.getElementById('ignoreCaseRegex').innerHTML = result;
-}
+});
 
-document.getElementById("extractMatches").addEventListener("click",extractMatchRegex);
-function extractMatchRegex(){
+document.getElementById("extractMatches").addEventListener("click", () => {
   let str = "cat dog dog cat squirell cat";
   // .match() extracts matches instead of true or false
   // '/g' returns all matches
   regex = /cat/g;
   result = str.match(regex);
   document.getElementById('extractMatches').innerHTML = result;
-}
+});
 
-document.getElementById('wildcardCharacter').addEventListener("click",wildcardRegex);
-function wildcardRegex(){
+document.getElementById('wildcardCharacter').addEventListener("click", () => {
+  //using . as a wildcard character
   let str = "cat sat log mat fit pat dat";
   let regex = /.at/g;
   let result = str.match(regex);
   document.getElementById('wildcardCharacter').innerHTML = result;
-}
-document.getElementById('matchSingleCharacters').addEventListener("click",matchSingleCharacters);
-function matchSingleCharacters(){
+});
+
+document.getElementById('matchSingleCharacters').addEventListener("click",() => {
+  //match single characters withing []
   let str = "baG big Bug beg bog";
   let regex = /b[aeiou]g/gi;
   let result = str.match(regex);
   document.getElementById('matchSingleCharacters').innerHTML = result;
-}
+});
 
- document.getElementById('matchLettersAlphabet').addEventListener("click",matchLettersOfAlphabet);
- function matchLettersOfAlphabet(){
+ document.getElementById('matchLettersAlphabet').addEventListener("click", () => {
    let str = "Hello, my name is jeff and I love Javascript.";
     //matches letters from a-z
     //space after a-z means include spaces
@@ -61,9 +59,9 @@ function matchSingleCharacters(){
     //.replace replaces commas with spaces
   result = result.replace(/,/g,"");
    document.getElementById('matchLettersAlphabet').innerHTML = result;
- }
-  document.getElementById('nonMatchingVowels').addEventListener('click', matchNonMatchingCharacters);
-  function matchNonMatchingCharacters(){
+ });
+
+  document.getElementById('nonMatchingVowels').addEventListener('click', () => {
   let str = "Here is a str full of text with lots of letters";
   // Remove letters aeiou with ^
   let regex = /[^aeiou]/gi;
@@ -72,4 +70,4 @@ function matchSingleCharacters(){
    //.replace replaces commas with spaces
   result = result.replace(/,/g,"");
   document.getElementById('nonMatchingVowels').innerHTML = result;
-}
+});
