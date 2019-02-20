3f92ef209e262899d25a314c50f505108fce9bf7
HarryTate <harryjamestate@gmail.com>
2019-02-20 21:42:34 +0000

    Further work on prototype objects

---
+++ js/prototype.js
@@ -0,0 +1,29 @@
+function Car(make, model, rightHandDrive, color, regYear) {
+    this.make = make,
+    this.model = model,
+    this.rightHandDrive = rightHandDrive,
+    this.color = color,
+    this.regYear = regYear
+}
+// Using prototype to call methods on instances of an object
+Car.prototype.carDetails = function(){
+    return `This car is a ${this.make} ${this.model}. It is ${this.color}`;
+}
+
+Car.prototype.carAge = function() {
+    let carAge = new Date().getFullYear() - this.regYear; 
+    return `This car is ${carAge} year old`;
+}
+
+Car.prototype.driverSide = function() {
+    if(this.rightHandDrive){
+        return 'Car is right hand drive';
+    } else {
+        return  'Car is left hand drive';
+    }
+}
+let car1 = new Car('Ford','Fiesta',true,'Orange',2005);
+let car2 = new Car('Vauxhall','Astra',true,'Black', 2010);
+let car3 = new Car('Kia','Sportage',false,'White',  2017);
+
+
