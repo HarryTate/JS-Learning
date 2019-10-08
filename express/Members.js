e25cba272c3878f7461b6076847a63f53607a3a6
HarryTate <harryjamestate@gmail.com>
2019-10-08 21:25:15 +0100

    Members ad middleware added

---
+++ express/Members.js
@@ -0,0 +1,22 @@
+const members = [
+  {
+    id: 1,
+    name: "James",
+    email: "james@gmail.com",
+    status: "active"
+  },
+  {
+    id: 2,
+    name: "Roger",
+    email: "roger@gmail.com",
+    status: "inactive"
+  },
+  {
+    id: 3,
+    name: "Beatrice",
+    email: "beatrice@gmail.com",
+    status: "inactive"
+  }
+];
+
+module.exports = members;
--- express/index.js
+++ express/index.js
@@ -1,33 +1,23 @@
 const express = require("express");
 const path = require("path");
+const members = require("./Members");
+const logger = require("./middleware/logger");
+
 const app = express();
 
-const members = [
-  {
-    id: 1,
-    name: "James",
-    email: "james@gmail.com",
-    status: "active"
-  },
-  {
-    id: 2,
-    name: "Roger",
-    email: "roger@gmail.com",
-    status: "inactive"
-  },
-  {
-    id: 3,
-    name: "Beatrice",
-    email: "beatrice@gmail.com",
-    status: "inactive"
-  }
-];
-/* app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-}); */
+// Init middleware
+// app.use(logger);
+
+//Gets all members
 app.get("/api/members", (req, res) => {
   res.json(members);
 });
+
+// Get Single member
+app.get("/api/members/:id", (req, res) => {
+  res.json(members.filter(member => member.id === parseInt(req.params.id)));
+});
+// Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 5000;
