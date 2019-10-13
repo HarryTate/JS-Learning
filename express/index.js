05e4e3058e529df6c25f6d75f4422908440bf75e
HarryTate <harryjamestate@gmail.com>
2019-10-13 21:52:53 +0100

    Added routes

--- express/index.js
+++ express/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const path = require("path");
-const members = require("./Members");
 const logger = require("./middleware/logger");
 
 const app = express();
@@ -8,17 +7,11 @@ const app = express();
 // Init middleware
 // app.use(logger);
 
-//Gets all members
-app.get("/api/members", (req, res) => {
-  res.json(members);
-});
-
-// Get Single member
-app.get("/api/members/:id", (req, res) => {
-  res.json(members.filter(member => member.id === parseInt(req.params.id)));
-});
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
+
+//Members API Routes
+app.use("/api/members", require("./routes/api/members"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
